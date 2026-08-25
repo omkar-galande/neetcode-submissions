aa8870a29d9fce8cec23552f56f1aaabe9118504
Omkar Galande <165264842+omkar-galande@users.noreply.github.com>
2026-08-25 15:17:03 +0530

    Add: valid-word-abbreviation - submission-0

---
+++ Data Structures & Algorithms/valid-word-abbreviation/submission-0.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean validWordAbbreviation(String word, String abbr) {
+        int i = 0;
+        int j = 0;
+
+        while(i < word.length() && j < abbr.length()){
+            char w_c = word.charAt(i);
+            char a_c = abbr.charAt(j);
+
+            if(Character.isDigit(a_c)){
+                if(a_c == '0'){
+                    return false;
+                }else{
+                    int curr = 0;
+
+                    while(j < abbr.length() && Character.isDigit(abbr.charAt(j)))
+                    {
+                        curr = curr * 10 + (abbr.charAt(j) - '0');
+                        j++;
+                    }
+                    i += curr;
+                }
+            }else{
+                if (a_c != w_c){
+                    return false;
+                }
+                i++;
+                j++;
+            }
+        }
+        return i == word.length() && j == abbr.length();
+    }
+}
